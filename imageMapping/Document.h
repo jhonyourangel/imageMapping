26e188caacaa2368370b7abecdf03cf615586471
AiU <AiU@AiUs-MacBook-Pro.local>
2014-05-16 12:44:38 +0200

    Initial Commit

---
+++ imageMapping/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  imageMapping
+//
+//  Created by AiU on 16/05/14.
+//  Copyright (c) 2014 AiU. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
---
+++ imageMapping/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  imageMapping
+//
+//  Created by AiU on 16/05/14.
+//  Copyright (c) 2014 AiU. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
